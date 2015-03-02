c694dd814f56380801128a3d1c68551220a4256b
Danny Nemer <hi@DannyNemer.com>
2015-03-01 22:34:39 -0600

    Use new `Parser` and `StateTable` with CLI

--- src/parser/CLI.js
+++ src/parser/CLI.js
@@ -1,12 +1,35 @@
-var StateTable = require('./stateTable')
-
 // If node-debug, pause for 1 second to attach debugger
 // Option --debug-brk (or -b) does this, but broken in node v0.12
 // Relies on breakpoints being set in inspector on previous run
 while (/bin/.test(process.argv[0]) && process.uptime() < 1) {}
 
-var grammar = require('../../grammar.json')
-var stateTable = new StateTable(grammar, '[start]')
+var grammar = require('../grammar.json')
+// var grammar = require('./original/gram0.json')
+
+if (!grammar.startSymbol) {
+	var nontermSyms = Object.keys(grammar)
+	grammar.startSymbol = '[start]'
+	grammar.terminals = []
+	grammar.nonterminals = []
+
+	nontermSyms.forEach(function (nontermSym) {
+		grammar[nontermSym].forEach(function (rule) {
+			var rules
+
+			if (rule.terminal) {
+				rules = grammar.terminals[nontermSym] || (grammar.terminals[nontermSym] = [])
+			} else {
+				rules = grammar.nonterminals[nontermSym] || (grammar.nonterminals[nontermSym] = [])
+			}
+
+			rules.push(rule)
+		})
+	})
+}
+
+
+var stateTable = new (require('./StateTable'))(grammar)
+stateTable.print()
 
 var readline = require('readline')
 var rl = readline.createInterface(process.stdin, process.stdout)
@@ -18,16 +41,19 @@ rl.on('line', function (line) {
 	console.log('query:', query)
 
 	try {
-		require('./aang').parse(query, stateTable)
+		var parser = new (require('./Parser'))(stateTable)
+		parser.parse(query)
+
+		parser.printForest()
+		parser.printStack()
+		if (parser.startNode) parser.printGraph()
 	} catch (e) {
 		// Remove portion of stack trace specific to Node
 		console.log()
 		console.log(e.stack)
 	}
 
-	delete require.cache[require.resolve('./aang')]
-	delete require.cache[require.resolve('./parser')]
-	delete require.cache[require.resolve('./matchTermSymbols')]
+	delete require.cache[require.resolve('./Parser')]
 
 	rl.prompt()
 })
