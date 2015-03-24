a9edeb648da14e0bd5867752f0b970f2b7e3b74b
Danny Nemer <hi@DannyNemer.com>
2015-03-24 17:45:30 -0500

    Use commands in CLI to toggle settings

--- src/parser/CLI.js
+++ src/parser/CLI.js
@@ -2,41 +2,73 @@
 // Option --debug-brk (or -b) does this, but broken in node v0.12
 // Relies on breakpoints being set in inspector on previous run
 while (/bin/.test(process.argv[0]) && process.uptime() < 1) {}
-var printTime = process.argv.indexOf('-t') !== -1
 
 var grammar = require('../grammar.json')
 
 var prevRSS = process.memoryUsage().rss
 var stateTable = new (require('./StateTable'))(grammar, '[start]')
 console.log((process.memoryUsage().rss - prevRSS) / 1e6 + ' MB')
-stateTable.print()
+// stateTable.print()
 
 var readline = require('readline')
 var rl = readline.createInterface(process.stdin, process.stdout)
 
 rl.prompt()
+rl.write('people who like repos liked by people who follow people I follow')
+
+var printTime = false
+var printStack = false
+var printForest = false
+var printGraph = false
+var parserPath = './Parser'
 
 rl.on('line', function (line) {
 	var query = line.trim()
-	console.log('query:', query)
 
-	try {
-		var parser = new (require('./Parser'))(stateTable)
+	if (query === '-t') {
+		printTime = !printTime
+		console.log('print time:', printTime)
+	} else if (query === '-s') {
+		printStack = !printStack
+		console.log('print stack:', printStack)
+	} else if (query === '-f') {
+		printForest = !printForest
+		console.log('print forest:', printForest)
+	} else if (query === '-g') {
+		printGraph = !printGraph
+		console.log('print graph:', printGraph)
+	} else if (query === '-p') {
+		parserPath = parserPath === './Parser' ? './Parser old.js' : './Parser'
+		console.log('parser path:', parserPath)
+	} else if (query === '-h') {
+		console.log('TOGGLES:')
+		console.log('-t  print time:', printTime)
+		console.log('-s  print stack:', printStack)
+		console.log('-f  print forest:', printForest)
+		console.log('-g  print graph:', printGraph)
+		console.log('-p  parser path:', parserPath)
+	} else {
+		try {
+			console.log('query:', query)
+			var parser = new (require(parserPath))(stateTable)
 
-		if (printTime) console.time('parse')
-		parser.parse(query)
-		if (printTime) console.timeEnd('parse')
+			if (printTime) console.time('parse')
+			parser.parse(query)
+			if (printTime) console.timeEnd('parse')
 
-		parser.printForest()
-		parser.printStack()
-		if (parser.startNode) parser.printNodeGraph(parser.startNode)
-	} catch (e) {
-		// Remove portion of stack trace specific to Node
-		console.log()
-		console.log(e.stack)
+			if (printForest) parser.printForest()
+			if (printStack) parser.printStack()
+			if (printGraph && parser.startNode) parser.printNodeGraph(parser.startNode)
+		} catch (e) {
+			// Remove portion of stack trace specific to Node
+			console.log()
+			console.log(e.stack)
+		}
+
+		delete require.cache[require.resolve(parserPath)]
+		delete require.cache[require.resolve('./vertTabCompare.js')]
+		delete require.cache[require.resolve('./BinaryHeap.js')]
 	}
-
-	delete require.cache[require.resolve('./Parser')]
 
 	rl.prompt()
 })
