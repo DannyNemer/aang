bf7a7059256f4e255ddf84fac69bbfd9e899c8a2
Danny Nemer <hi@DannyNemer.com>
2015-05-02 20:00:04 -0500

    Add prepositions for "issues with <int> comments"

--- src/grammar/rules/prepositions.js
+++ src/grammar/rules/prepositions.js
@@ -22,3 +22,39 @@ this.possessorSpecial = g.addWord({
 	symbol: new g.Symbol(prepStr, 'possessor', 'special'),
 	accepted: [ 'of' ]
 })
+
+// (issues) with (n comments)
+this.possessed = g.addWord({
+	symbol: new g.Symbol(prepStr, 'possessed'),
+	insertionCost: 0.5,
+	accepted: [ 'with' ],
+	substitutions: [ 'have|having|containing' ]
+})
+
+// (issues with n) to (i comments)
+this.end = g.addWord({
+	symbol: new g.Symbol(prepStr, 'end'),
+	insertionCost: 0.5,
+	accepted: [ 'to' ]
+})
+
+// (issues with) between (n and i comments)
+this.between = g.addWord({
+	symbol: new g.Symbol(prepStr, 'between'),
+	insertionCost: 2,
+	accepted: [ 'between' ]
+})
+
+// (issues with) over (n comments)
+this.over = g.addWord({
+	symbol: new g.Symbol(prepStr, 'over'),
+	insertionCost: 0.5,
+	accepted: [ 'over', 'above|beyond', 'more than', '>' ]
+})
+
+// (issues with) under (n comments)
+this.under = g.addWord({
+	symbol: new g.Symbol(prepStr, 'under'),
+	insertionCost: 0.5,
+	accepted: [ 'under', 'below', 'less than', '<' ]
+})
