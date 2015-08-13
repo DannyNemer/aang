c80ff36c059bf70565bda8cfc72ff513a69b954a
Danny Nemer <hi@DannyNemer.com>
2015-08-13 18:38:05 -0400

    Move test queries to 'testQueries.js'

--- src/parser/CLI.js
+++ src/parser/CLI.js
@@ -5,6 +5,8 @@ var parserPath = './Parser.js'
 var forestSearchPath = './forestSearch.js'
 var stateTablePath = './StateTable.js'
 
+var testQueries = require('./testQueries')
+
 var stateTable = buildStateTable()
 
 
@@ -100,9 +102,10 @@ function runCommand(input) {
 		else console.time('test')
 
 		var i = 0
+		var queries = testQueries.basic
 		while (i++ < testRuns) {
-			for (var q = 0, testQueriesLen = testQueries.length; q < testQueriesLen; ++q) {
-				parse(testQueries[q], 50)
+			for (var q = 0, queriesLen = queries.length; q < queriesLen; ++q) {
+				parse(queries[q], 50)
 			}
 		}
 
@@ -142,7 +145,7 @@ function runCommand(input) {
 		var prevSettingPrintOutput = printOutput
 		printOutput = false
 
-		conjugationTestQueries.forEach(function (query) {
+		testQueries.conjugation.forEach(function (query) {
 			var trees = parse(query, 1)
 			if (!trees || trees[0].text !== query) {
 				util.printErr('Expected', query)
@@ -314,169 +317,3 @@ function buildStateTable() {
 function deleteModuleCaches() {
 	util.deleteModuleCache(parserPath, forestSearchPath, stateTablePath, './BinaryHeap.js', '../grammar/semantic.js', './calcHeuristics.js')
 }
-
-
-var testQueries = [
-	'repos I have liked',
-	'repos created by me and my followers',
-	'repos I and my followers created',
-	'people who like my repos liked by people who follow people I follow',
-	'people who like repos',
-	'repos been followers',
-	'repos been followers people who like repos that I have',
-	'repos people who like and created',
-	'repos that have been created by people and like and I contributed to',
-	'repos that are repos',
-	'my followers who are my followers', // intentionally fails
-	'my followers who are followers of followers of mine',
-	'my followers who are followers of followers of mine who liked that repos contributed to of mine',
-	'repos',
-	'people',
-	'people who created my repos and my pull requests',
-	'people pull requests like repos I like',
-	'repos liked by followers of followers of mine',
-	'repos liked by me and my followers',
-	'repos liked by me and my followers and people who like JavaScript repos liked by me and my followers',
-	'my repos that are JavaScript repos',
-	'my JavaScript repos',
-	'repos that are written in JavaScript',
-	'my JavaScript repos that are written in JavaScript', // intentionally fails
-	'issues assigned to me I opened and am mentioned in',
-	'people who are assigned to my issues and follow people who contributed to repos I created and are mentioned in pull requests of mine',
-	'people who are mentioned in my issues and pull requests',
-	'people who are assigned to my issues and my pull requests',
-	'people mentioned in my issues and my pull requests',
-	'my {left-stop-words} repos',
-	'my {left-stop-words} JavaScript repos',
-	'my JavaScript {left-stop-words} repos',
-	'my {left-stop-words} JavaScript {left-stop-words} repos',
-	'my {left-stop-words} {left-stop-words} repos',
-	'open issues',
-	'issues that are open',
-	'people assigned to my closed issues',
-	'contributors to my repos',
-	'contributors to my repos and repos of mine',
-	'likers of my repos',
-	'creators of repos I like',
-	'likers of repos I like and repos I contributed to',
-	'creators of repos I like and repos I contributed to',
-	'creators of repos I like and pull requests I am mentioned in', // unimplemented
-	'openers of closed issues that mention people I and my followers follow',
-	'people who are not followers of mine',
-	'people who have not been followed by me',
-	'issues that are not open',
-	'people who are not followed by me',
-	'people I do not follow', // unimplemented
-	'people who follow me and do not follow Danny',
-	'issues I am assigned to',
-	'{left-stop-words} open issues that are never assigned to people who {pre-filter-stop-words} follow me',
-	'repos of people who follow me',
-	'followers of mine and people who follow me',
-	'repos of mine and people who follow me',
-	'repos I like or my followers likes',
-	'repos I or my followers likes',
-	'repos created by people I follow and Danny follows',
-	'repos created by people I and Danny follow',
-	'people who follow people',
-	'people followed by myself',
-	'people who follow I',
-	'people me follows',
-	'openers of my closed issues that mention me people who follow me and my followers follow follow',
-	'repos people my followers follow like',
-	'people people Danny follows follow',
-	'repos people people Danny follows follow created Danny likes',
-	'followers of my followers who are followers of mine my followers who created repositories of my followers followers of mine who I follow like that are repos I contributed to follow',
-	'repos contributed to by me',
-	'repos to by me', // intentionally wrong
-	'repos liked contributed to by me', // intentionally wrong
-	'repos by me', // should insert 'liked'
-	'issues opened by me assigned to me', // transposition
-	'issues with 22 comments',
-	'issues assigned to me with 22 comments',
-	'issues with 20 to 34 comments I opened',
-	'issues that are with between 20 comments and 34 comments',
-	'issues with 3 comments to 3 comments',
-	'issues with 3 to 3 comments',
-	'issues with less than 2 > 4 comments',
-	'JavaScript repos liked by me and my followers',
-	'repos with between 22 and 23 likes',
-	'repos created in 2014',
-	'repos created 2014',
-	'repos created in June 2014',
-	'repos created in June 24 2014',
-	'repos created this year',
-	'repos created after this year',
-	'repos created between last year and this year',
-	'repos created from 2012 to 2014',
-	'repos created before June 20 2000 and after this year',
-	'people I and Danny follows', // intentionally incorrect
-	'people people Danny follow and Danny follows', // intentionally incorrect
-	'likers of my repos I contributed to that I like and my repos I contributed to',
-	'my GitHub repos liked by my github followers',
-	'my followers\' repos',
-	'my {left-stop-words} followers\' repos',
-	'my {left-stop-words} followers\' {left-stop-words} repos',
-	'Danny\'s followers\' followers',
-	'repos of Danny\'s',
-	'Danny\'s followers',
-	'people who are male',
-	'female people',
-	'female people who are male',
-	'male people who are not male',
-	'my male followers',
-	'Danny\'s male followers',
-	'my followers\' female followers',
-	'my female followers\' female followers',
-	'my female followers\' female followers who are not male',
-	'REPOS i LiKe',
-	'repos that are not created today liked by me',
-	'people who are not my followers',
-	'repos not created today not liked by me', // should produce results
-	'repos created today created yesterday', // intentionally incorrect
-	'women',
-	'men I follow',
-	'people who are males',
-	'male women',
-	'female people who are women',
-	'repos that are created by me and are not created by me',
-	'repos that are not created by me and are created by me',
-	'people people I follow who Danny follow and follow Danny follow',
-	'repos people I follow created that Danny like',
-	'issues assigned to me Danny opened and Aang are mentioned in',
-	'people {user} follows', // intentionally wrong
-	'people I and {user} follow', // intentionally wrong
-	'issues with <int> comments', // intentionally wrong
-	'my repos me people who follow my followers have been and',
-	'people my followers who created repositories of my followers followers of mine who I follow like follow',
-	'contributors to my repos or repos I like or Danny likes',
-	'repos I like or I created',
-	'repos I or Danny like',
-	'repos I or Danny or my followers like',
-	'repos I or Danny and my followers like',
-	'repos I or Danny created',
-	// 'repos danny by me', // intentionally wrong - requires implementation of deletions
-	// 'repos danny by me danny', // intentionally wrong - requires implementation of deletions
-	// 'repos that I created I like', // intentionally wrong
-	// 'people who I follow Danny follows', // intentionally wrong
-	// 'pull requests of mine created by my followers' // reaches startNode but produces no legal trees
-	// 'my followers who created pull requests of mine my followers who created repositories followers of mine mentioned in issues of my followers who I follow like that are repos created by me I contributed to am mentioned in that I am mentioned in',
-	// illegal, but takes a long time for search to fail (produce no results):
-	// 'my repositories people who created my repos created'
-	// 'my' // doesn't work
-	// 'people who like my repos liked by people who follow me that people I follow created'
-]
-
-// The first result for these queries must match the input exactly
-var conjugationTestQueries = [
-	'people I follow',
-	'people Danny follows',
-	'people I and Danny follow',
-	'people Danny and I follow',
-	'people people Danny follows follow',
-	'people people Danny follows and Danny follow',
-	'repos I have liked',
-	'repos Danny has liked',
-	'issues assigned to me Danny opened and Aang is mentioned in',
-	'repos people I follow created that Danny likes',
-	'people people I follow who Danny follows and follow Danny follow'
-]
