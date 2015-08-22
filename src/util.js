faeabd86faa3169e3c28e9a897360b2cb367fdde
Danny Nemer <hi@DannyNemer.com>
2015-08-22 10:37:03 -0400

    Reorder functions in 'util.js'

--- src/util.js
+++ src/util.js
@@ -96,6 +96,137 @@ exports.illFormedOpts = function (schema, opts) {
 }
 
 /**
+ * Synchronously writes the output of a function to a file instead of the console. Overwrites the file if it already exists.
+ *
+ * @param {String} path The path where to write output.
+ * @param {Function} callback The function producing output.
+ * @return {Mixed} The value returned by `callback`, if any.
+ * @example
+ * // Prints to console
+ * console.log('Begin output to file')
+ *
+ * // Redirects process output from console to file
+ * dannyUtil.redirectOutputToFile('~/Desktop/out.txt', function () {
+ *   // Writes to '~/Desktop/out.txt'
+ *   console.log('Numbers:')
+ *   for (var i = 0; i < 100; ++i) {
+ *     console.log(i)
+ *   }
+ * })
+ * // Restores output to console and prints: "Output saved to: ~/Desktop/out.txt"
+ *
+ * // Prints to console (after restoring output)
+ * console.log('Output to file complete')
+ */
+exports.redirectOutputToFile = function (path, callback) {
+	// Expand '~' if present
+	path = exports.expandHomeDir(path)
+
+	// Create file if does not exist, overwrite existing file if exists, or throw an error if `path` is a directory
+	fs.writeFileSync(path)
+
+	// Redirect `process.stdout` to `path`
+	var writable = fs.createWriteStream(path)
+	var oldWrite = process.stdout.write
+	process.stdout.write = function () {
+		writable.write.apply(writable, arguments)
+	}
+
+	// Write output to `path`
+	var returnVal = callback()
+
+	// Restore `process.stdout`
+	process.stdout.write = oldWrite
+
+	console.log('Output saved to:', path)
+
+	return returnVal
+}
+
+/**
+ * Writes an object to a JSON file.
+ *
+ * @param {String} path The path to write file.
+ * @param {Object} obj The object to save to file.
+ */
+exports.writeJSONFile = function (path, obj) {
+	fs.writeFileSync(exports.expandHomeDir(path), JSON.stringify(obj, function (key, val) {
+		// Convert RegExp to strings for `JSON.stringify()`
+		return val instanceof RegExp ? val.source : val
+	}, '\t'))
+
+	console.log('File saved:', path)
+}
+
+/**
+ * Replaces `'~'` in a path (if present and at the path's start) with the home directory path.
+ *
+ * @param {String} path The file path.
+ * @return {String} `path` with '~' (if present) replaced with the home directory path.
+ * @example
+ * dannyUtil.expandHomeDir('~/Desktop') // -> '/Users/Danny/Desktop'
+ */
+exports.expandHomeDir = function (path) {
+	return path.replace(/^~/, process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'])
+}
+
+/**
+ * Executes the passed function within a `try` block.
+ * Removes parentheses from error stack for iTerm open-file-path shortcut.
+ * Colors error type name red (e.g., `ReferenceError`).
+ *
+ * @param {Function} callback The function to execute within a `try` block.
+ * @return {Mixed} The value returned by `callback`, if any.
+ */
+exports.tryCatchWrapper = function (callback) {
+	try {
+		return callback()
+	} catch (e) {
+		console.log()
+
+		if (e.stack) {
+			// Error message without source code (if present)
+			var message = e.message.split('\n').pop()
+
+			e.stack.split('\n').forEach(function (stackFrame) {
+				if (e.message.indexOf(stackFrame) !== -1) {
+					console.log(stackFrame)
+				} else if (stackFrame.indexOf(message) !== -1) {
+					// Color error type name red
+					console.log(stackFrame.replace(e.name, colors.red(e.name)))
+				} else {
+					// Remove parentheses of file paths for iTerm open-file-path shortcut
+					console.log(stackFrame.replace(/[()]/g, ''))
+				}
+			})
+		} else {
+			console.log(e)
+		}
+	}
+}
+
+/**
+ * Deletes modules from cache, forcing them to be reloaded at next `require()` call. Without removing a module from cache, subsequent `require()` calls to the same module will not enable changes to its file(s).
+ * This is useful for enabling changes on a server without restarting the server.
+ *
+ * @param {...String} pathN The paths of modules to remove from cache.
+ * @example
+ * // Load module
+ * var myModule = require('./myModule.js')
+ *
+ * // Remove module from cache
+ * dannyUtil.deleteModuleCache('./myModule.js')
+ *
+ * // Load module again, enabling changes to './myModule.js'
+ * myModule = require('./myModule.js')
+ */
+exports.deleteModuleCache = function () {
+	Array.prototype.slice.call(arguments).forEach(function (path) {
+		delete require.cache[fs.realpathSync(path)]
+	})
+}
+
+/**
  * Gets the file path and line number of the first item in the stack of the parent module from where this function was called. This is useful for logging where an object is instantiated.
  *
  * @param {Boolean} getCallingLine If `true`, return line of where this function was called, else the line of the parent module.
@@ -127,46 +258,6 @@ exports.getLine = function (getCallingLine) {
 	// Could not find line in stack for file from which function calling `getLine()` was called
 	// exports.printErr('Sought-after line not found in stack trace (trace limited to 10 most recent)')
 	// exports.logTrace()
-}
-
-/**
- * Performs a shallow comparison between two arrays to determine if they are equivalent.
- *
- * @param {Array} a The array to compare.
- * @param {Array} b The other array to compare.
- * @return {Boolean} `true` if the arrays are equivalent, else `false`.
- * @example
- * dannyUtil.arraysEqual([], []) // -> true
- *
- * dannyUtil.arraysEqual([1, 2, 3, 'danny'], [1, 2, 3, 'danny']) // -> true
- *
- * dannyUtil.arraysEqual([ false, true ], [ true ]) // -> false
- *
- * // A shallow comparison will not compare object properties
- * var objA = { prop: 'val' }
- * var objB = { prop: 'val' }
- * dannyUtil.arraysEqual([ 1, 2, objA ], [ 1, 2, objB ]) // -> false
- *
- * // Rather, objects are only equal if they are the same instance
- * dannyUtil.arraysEqual([ objA, objB ], [ objA, objB ]) // -> true
- */
-exports.arraysEqual = function (a, b) {
-	// Identical arrays (or, both undefined)
-	if (a === b) return true
-
-	// One of two is undefined
-	if (!a || !b) return false
-
-	var i = a.length
-
-	// Different lengths
-	if (i !== b.length) return false
-
-	while (i--) {
-		if (a[i] !== b[i]) return false
-	}
-
-	return true
 }
 
 /**
@@ -206,118 +297,6 @@ function prettyPrint(args, opts) {
 			console.dir(arg, opts)
 		}
 	})
-}
-
- /**
-	* Key-value map used by `time()`.
-	*
-	* @private
-	* @type Map
-	*/
-var _times = new Map()
-
-/**
- * Starts a high-resolution timer (with precision in nanoseconds) identified by `label`. Use `dannyUtil.timeEnd(label)` to print the timer's current value.
- *
- * @param {String} label The identifier of the timer.
- * @example
- * // Start timer
- * dannyUtil.time('my test')
- *
- * // ...stuff...
- *
- * // Prints "my test: 23.264491ms"
- * dannyUtil.timeEnd('my test')
- *
- * // ...more stuff...
- *
- * // Prints "my test: 36.183837ms"
- * dannyUtil.timeEnd('my test')
- */
-exports.time = function (label) {
-	_times.set(label, process.hrtime())
-}
-
-/**
- * Prints the current high-resolution value of a timer initiated with `dannyUtil.time(label)`.
- *
- * @param {String} label The identifier of the timer.
- */
-exports.timeEnd = function (label) {
-	var time = _times.get(label)
-
-	if (!time) {
-		throw new Error('No such label:', label)
-	}
-
-	var duration = process.hrtime(time)
-	console.log('%s: %dms', label, duration[0] * 1e3 + duration[1] / 1e6)
-}
-
-/**
- * Prints calling file path and line number to mark reaching a section of code, prepended by `msg`.
- *
- * @param {String} [msg] The optional message to prepend line.
- */
-exports.assert = function (msg) {
-	console.log(colors.red((msg || 'Reached') + ':'), exports.getLine(true))
-}
-
-/**
- * Prints calling file path and line number if `value` is truthy, prepended by `msg`.
- *
- * @param {Boolean} value The value to check if truthy.
- * @param {String} [msg] The optional message to prepend line.
- */
-exports.assertTrue = function (value, msg) {
-	if (value) exports.assert(msg)
-}
-
-/**
- * Key-value map used by `dannyUtil.count()`.
- *
- * @private
- * @type Map
- */
-var _counts = new Map()
-
-/**
- * Counts the number of times a section of code is reached, identified by `label`. Use `dannyUtil.countEnd(label)` to print value. This is useful for profiling complex programs.
- *
- * @param {String} label The id to refer to a section of code.
- */
-exports.count = function (label) {
-	var val = _counts.get(label) || 0
-	_counts.set(label, val + 1)
-}
-
-/**
- * Prints the number of calls of `dannyUtil.count(label)`.
- * Resets the count of calls to `label` when called.
- *
- * @param {String} label The id to refer to calls to `dannyUtil.count()`.
- */
-exports.countEnd = function (label) {
-	// Print even if no value to acknowledge never being reached
-	var count = _counts.get(label) || 0
-	console.log('%s: %d', label === undefined ? 'count' : label, count)
-
-	// Reset count
-	_counts.delete(label)
-}
-
-/**
- * Prints the values of all counters used on `dannyUtil.count()`.
- * Will not print counters that are never reached (and never have their keys initialized).
- * Reset all counts.
- */
-exports.countEndAll = function () {
-	_counts.forEach(function(count, label) {
-		console.log(label + ':', count)
-	})
-
-	// Reset all counts
-	_counts.clear()
 }
 
 /**
@@ -398,74 +377,155 @@ exports.logTrace = function (msg) {
 }
 
 /**
- * Writes an object to a JSON file.
+ * Prints calling file path and line number to mark reaching a section of code, prepended by `msg`.
  *
- * @param {String} path The path to write file.
- * @param {Object} obj The object to save to file.
+ * @param {String} [msg] The optional message to prepend line.
  */
-exports.writeJSONFile = function (path, obj) {
-	fs.writeFileSync(exports.expandHomeDir(path), JSON.stringify(obj, function (key, val) {
-		// Convert RegExp to strings for `JSON.stringify()`
-		return val instanceof RegExp ? val.source : val
-	}, '\t'))
-
-	console.log('File saved:', path)
+exports.assert = function (msg) {
+	console.log(colors.red((msg || 'Reached') + ':'), exports.getLine(true))
 }
 
 /**
- * Executes the passed function within a `try` block.
- * Removes parentheses from error stack for iTerm open-file-path shortcut.
- * Colors error type name red (e.g., 'ReferenceError').
+ * Prints calling file path and line number if `value` is truthy, prepended by `msg`.
  *
- * @param {Function} callback The function to execute within a `try` block.
- * @return {Mixed} The value returned by `callback`, if any.
+ * @param {Boolean} value The value to check if truthy.
+ * @param {String} [msg] The optional message to prepend line.
  */
-exports.tryCatchWrapper = function (callback) {
-	try {
-		return callback()
-	} catch (e) {
-		console.log()
-
-		if (e.stack) {
-			// Error message without source code (if present)
-			var message = e.message.split('\n').pop()
-
-			e.stack.split('\n').forEach(function (stackFrame) {
-				if (e.message.indexOf(stackFrame) !== -1) {
-					console.log(stackFrame)
-				} else if (stackFrame.indexOf(message) !== -1) {
-					// Color error type name red
-					console.log(stackFrame.replace(e.name, colors.red(e.name)))
-				} else {
-					// Remove parentheses of file paths for iTerm open-file-path shortcut
-					console.log(stackFrame.replace(/[()]/g, ''))
-				}
-			})
-		} else {
-			console.log(e)
-		}
-	}
+exports.assertTrue = function (value, msg) {
+	if (value) exports.assert(msg)
 }
 
+ /**
+	* Key-value map used by `time()`.
+	*
+	* @private
+	* @type Map
+	*/
+var _times = new Map()
+
 /**
- * Deletes modules from cache, forcing them to be reloaded at next `require()` call. Without removing a module from cache, subsequent `require()` calls to the same module will not enable changes to its file(s).
- * This is useful for enabling changes on a server without restarting the server.
+ * Starts a high-resolution timer (with precision in nanoseconds) identified by `label`. Use `dannyUtil.timeEnd(label)` to print the timer's current value.
  *
- * @param {...String} pathN The paths of modules to remove from cache.
+ * @param {String} label The identifier of the timer.
  * @example
- * // Load module
- * var myModule = require('./myModule.js')
+ * // Start timer
+ * dannyUtil.time('my test')
  *
- * // Remove module from cache
- * dannyUtil.deleteModuleCache('./myModule.js')
+ * // ...stuff...
  *
- * // Load module again, enabling changes to './myModule.js'
- * myModule = require('./myModule.js')
+ * // Prints "my test: 23.264491ms"
+ * dannyUtil.timeEnd('my test')
+ *
+ * // ...more stuff...
+ *
+ * // Prints "my test: 36.183837ms"
+ * dannyUtil.timeEnd('my test')
  */
-exports.deleteModuleCache = function () {
-	Array.prototype.slice.call(arguments).forEach(function (path) {
-		delete require.cache[fs.realpathSync(path)]
+exports.time = function (label) {
+	_times.set(label, process.hrtime())
+}
+
+/**
+ * Prints the current high-resolution value of a timer initiated with `dannyUtil.time(label)`.
+ *
+ * @param {String} label The identifier of the timer.
+ */
+exports.timeEnd = function (label) {
+	var time = _times.get(label)
+
+	if (!time) {
+		throw new Error('No such label:', label)
+	}
+
+	var duration = process.hrtime(time)
+	console.log('%s: %dms', label, duration[0] * 1e3 + duration[1] / 1e6)
+}
+
+/**
+ * Key-value map used by `dannyUtil.count()`.
+ *
+ * @private
+ * @type Map
+ */
+var _counts = new Map()
+
+/**
+ * Counts the number of times a section of code is reached, identified by `label`. Use `dannyUtil.countEnd(label)` to print value. This is useful for profiling complex programs.
+ *
+ * @param {String} label The id to refer to a section of code.
+ */
+exports.count = function (label) {
+	var val = _counts.get(label) || 0
+	_counts.set(label, val + 1)
+}
+
+/**
+ * Prints the number of calls of `dannyUtil.count(label)`.
+ * Resets the count of calls to `label` when called.
+ *
+ * @param {String} label The id to refer to calls to `dannyUtil.count()`.
+ */
+exports.countEnd = function (label) {
+	// Print even if no value to acknowledge never being reached
+	var count = _counts.get(label) || 0
+	console.log('%s: %d', label === undefined ? 'count' : label, count)
+
+	// Reset count
+	_counts.delete(label)
+}
+
+/**
+ * Prints the values of all counters used on `dannyUtil.count()`.
+ * Will not print counters that are never reached (and never have their keys initialized).
+ * Reset all counts.
+ */
+exports.countEndAll = function () {
+	_counts.forEach(function(count, label) {
+		console.log(label + ':', count)
 	})
+
+	// Reset all counts
+	_counts.clear()
+}
+
+/**
+ * Performs a shallow comparison between two arrays to determine if they are equivalent.
+ *
+ * @param {Array} a The array to compare.
+ * @param {Array} b The other array to compare.
+ * @return {Boolean} `true` if the arrays are equivalent, else `false`.
+ * @example
+ * dannyUtil.arraysEqual([], []) // -> true
+ *
+ * dannyUtil.arraysEqual([1, 2, 3, 'danny'], [1, 2, 3, 'danny']) // -> true
+ *
+ * dannyUtil.arraysEqual([ false, true ], [ true ]) // -> false
+ *
+ * // A shallow comparison will not compare object properties
+ * var objA = { prop: 'val' }
+ * var objB = { prop: 'val' }
+ * dannyUtil.arraysEqual([ 1, 2, objA ], [ 1, 2, objB ]) // -> false
+ *
+ * // Rather, objects are only equal if they are the same instance
+ * dannyUtil.arraysEqual([ objA, objB ], [ objA, objB ]) // -> true
+ */
+exports.arraysEqual = function (a, b) {
+	// Identical arrays (or, both undefined)
+	if (a === b) return true
+
+	// One of two is undefined
+	if (!a || !b) return false
+
+	var i = a.length
+
+	// Different lengths
+	if (i !== b.length) return false
+
+	while (i--) {
+		if (a[i] !== b[i]) return false
+	}
+
+	return true
 }
 
 /**
@@ -494,64 +554,4 @@ exports.dashedToCamelCase = function (dashedString) {
 	return dashedString.replace(/-(\w)/g, function (match, group1) {
 		return group1.toUpperCase()
 	})
-}
-
-/**
- * Synchronously writes the output of a function to a file instead of the console. Overwrites the file if it already exists.
- *
- * @param {String} path The path where to write output.
- * @param {Function} callback The function producing output.
- * @return {Mixed} The value returned by `callback`, if any.
- * @example
- * // Prints to console
- * console.log('Begin output to file')
- *
- * // Redirects process output from console to file
- * dannyUtil.redirectOutputToFile('~/Desktop/out.txt', function () {
- *   // Writes to '~/Desktop/out.txt'
- *   console.log('Numbers:')
- *   for (var i = 0; i < 100; ++i) {
- *     console.log(i)
- *   }
- * })
- * // Restores output to console and prints: "Output saved to: ~/Desktop/out.txt"
- *
- * // Prints to console (after restoring output)
- * console.log('Output to file complete')
- */
-exports.redirectOutputToFile = function (path, callback) {
-	// Expand '~' if present
-	path = exports.expandHomeDir(path)
-
-	// Create file if does not exist, overwrite existing file if exists, or throw an error if `path` is a directory
-	fs.writeFileSync(path)
-
-	// Redirect `process.stdout` to `path`
-	var writable = fs.createWriteStream(path)
-	var oldWrite = process.stdout.write
-	process.stdout.write = function () {
-		writable.write.apply(writable, arguments)
-	}
-
-	// Write output to `path`
-	var returnVal = callback()
-
-	// Restore `process.stdout`
-	process.stdout.write = oldWrite
-
-	console.log('Output saved to:', path)
-
-	return returnVal
-}
-
-/**
- * Replaces `'~'` in a path (if present and at the path's start) with the home directory path.
- *
- * @param {String} path The file path.
- * @return {String} `path` with '~' (if present) replaced with the home directory path.
- * @example
- * dannyUtil.expandHomeDir('~/Desktop') // -> '/Users/Danny/Desktop'
- */
-exports.expandHomeDir = function (path) {
-	return path.replace(/^~/, process.env[process.platform === 'win32' ? 'USERPROFILE' : 'HOME'])
 }
