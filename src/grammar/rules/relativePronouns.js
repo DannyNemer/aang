f1adf6eb133ed3a8e68bed619f54c024c5db2590
Danny Nemer <hi@DannyNemer.com>
2015-09-06 14:59:37 -0400

    Add substitutions to relative pronouns

--- src/grammar/rules/relativePronouns.js
+++ src/grammar/rules/relativePronouns.js
@@ -6,6 +6,7 @@ exports.who = g.newSymbol('who')
 exports.who.addWord({
 	insertionCost: 0.1,
 	accepted: [ 'who', 'that' ],
+	substitutions: [ 'at|about|by|for|from|in|of|on|to|with that|what|when|where|which|who|whom|whose|why' ],
 })
 
 // (repos) that (are liked by me)
@@ -13,4 +14,5 @@ exports.that = g.newSymbol('that')
 exports.that.addWord({
 	insertionCost: 0.1,
 	accepted: [ 'that', 'which' ],
+	substitutions: [ 'at|about|by|for|from|in|of|on|to|with that|what|when|where|which|who|whom|whose|why' ],
 })
